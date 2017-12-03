863f0ae833a3e73b319e730110a7fcf5881b5b6a
Ryan Calingo <gryan.calingo@gmail.com>
2017-12-02 20:51:30 -0500

    game scripts

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,26 @@
 // SCRIPTS
+
+var game_rpg = {
+
+	initGame: function() {
+		this.gameCache();
+	},
+	gameCache: function() {
+		this.$game = $("#game_rpg");
+		this.$champions = this.$game.find("#character");
+		this.$challengers = this.$game.find("#challengers");
+		this.$attack_btn = this.$game.find("#attack_btn");
+
+		this.$defender = this.$game.find("#defender");
+
+		this.$results = this.$game.find("#results");
+
+		console.log(this.$champions);
+	},
+	bindEvents: function() {
+	},
+	champion: function() {
+
+	}
+}
+game_rpg.initGame();
